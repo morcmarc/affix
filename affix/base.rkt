b8fa60680d0764a5a410c4c955d7db96e7aaa77b
Marcell Jusztin <marcell.jusztin@infinityworks.com>
2018-10-17 15:29:52 +0100

    Rewrite defstate macro to use define-syntax-rule and take a start and stop function as arguments

--- affix/base.rkt
+++ affix/base.rkt
@@ -8,20 +8,31 @@
   [start state arg]
   [stop state])
 
-(define-syntax (defstate stx)
-  (let* ([xs (syntax->list stx)]
-         [name (cadr xs)])
-    (datum->syntax stx `(struct (unquote name) ()
-                          #:methods gen:state
-                          [(define (start state args)
-                              "starting")
-                           (define (stop state)
-                              "stopping")]))))
+;; (define-syntax (defstate stx)
+;;   (let* ([xs (syntax->list stx)]
+;;          [name (cadr xs)])
+;;     (datum->syntax stx `(struct (unquote name) ()
+;;                           #:methods gen:state
+;;                           [(define (start state args)
+;;                              "starting")
+;;                            (define (stop state)
+;;                              "stopping")]))))
+
+(define-syntax-rule (defstate name start-fn stop-fn)
+  (struct name ()
+    #:methods gen:state
+    [(define (start state args)
+       (start-fn state args))
+     (define (stop state)
+       (stop-fn state))]))
 
 (module+ test
   (require rackunit)
 
-  (defstate my-test)
+  (defstate my-test
+    (lambda (state args) "starting")
+    (lambda (state) "stopping"))
   (define m (my-test))
 
-  (check-eq? "starting" (start m #f)))
+  (check-eq? "starting" (start m #f))
+  (check-eq? "stopping" (stop m)))
