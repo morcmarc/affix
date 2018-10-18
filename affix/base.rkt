8d78421a415f5639d6a3218f1c2ab90053e4052b
Marcell Jusztin <marcell.jusztin@infinityworks.com>
2018-10-18 11:00:26 +0100

    Sketch out optional keyword arguments

--- affix/base.rkt
+++ affix/base.rkt
@@ -1,12 +1,13 @@
 #lang racket
 
-(require racket/generic)
+(require racket/generic
+         (for-syntax racket/base
+                     syntax/parse))
 
 (provide gen:state state/c state?)
 
 (define-generics state
-  [start state arg]
-  [stop state])
+  [start state arg])
 
 ;; (define-syntax (defstate stx)
 ;;   (let* ([xs (syntax->list stx)]
@@ -18,21 +19,37 @@
 ;;                            (define (stop state)
 ;;                              "stopping")]))))
 
-(define-syntax-rule (defstate name start-fn stop-fn)
-  (struct name ()
-    #:methods gen:state
-    [(define (start state args)
-       (start-fn state args))
-     (define (stop state)
-       (stop-fn state))]))
+(define-syntax (defstate stx)
+  (syntax-parse stx
+    [(defstate id (~optional (~seq #:start start-fn:expr)))
+     (with-syntax ([start-fn? (if (attribute start-fn) #'#t #'#f)]
+                   [start-fn (or (attribute start-fn) #'#f)])
+       #'(defstate* id start-fn? start-fn))]))
+
+(define-syntax defstate*
+  (syntax-rules ()
+    [(defstate id #t start-fn)
+     (struct id ()
+       #:methods gen:state
+       [(define (start state args)
+          (start-fn state args))])]
+    [(defstate id #f _)
+     (struct id ()
+       #:methods gen:state
+       [(define (start state args)
+          "starting")])]
+    [(defstate id _)
+     (struct id ())]))
 
 (module+ test
   (require rackunit)
 
   (defstate my-test
-    (lambda (state args) "starting")
-    (lambda (state) "stopping"))
-  (define m (my-test))
+    #:start (lambda (state args) "custom starting"))
+  (defstate my-test-2)
+  
+  (define m-1 (my-test))
+  (define m-2 (my-test-2))
 
-  (check-eq? "starting" (start m #f))
-  (check-eq? "stopping" (stop m)))
+  (check-eq? "custom starting" (start m-1 #f))
+  (check-eq? "starting" (start m-2 #f)))
