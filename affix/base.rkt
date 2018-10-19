0803549ddcbda9025dd825d3db9c13e316a78105
Marcell Jusztin <marcell.jusztin@infinityworks.com>
2018-10-19 15:10:18 +0100

    Sort-of-working arguments (they can be in any order now, but mandatory for some reasons)

--- affix/base.rkt
+++ affix/base.rkt
@@ -1,55 +1,40 @@
 #lang racket
 
 (require racket/generic
-         (for-syntax racket/base
-                     syntax/parse))
+         (for-syntax racket/base syntax/parse))
 
-(provide gen:state state/c state?)
 
 (define-generics state
-  [start state arg])
-
-;; (define-syntax (defstate stx)
-;;   (let* ([xs (syntax->list stx)]
-;;          [name (cadr xs)])
-;;     (datum->syntax stx `(struct (unquote name) ()
-;;                           #:methods gen:state
-;;                           [(define (start state args)
-;;                              "starting")
-;;                            (define (stop state)
-;;                              "stopping")]))))
+  [start state arg]
+  [stop state])
 
 (define-syntax (defstate stx)
   (syntax-parse stx
-    [(defstate id (~optional (~seq #:start start-fn:expr)))
-     (with-syntax ([start-fn? (if (attribute start-fn) #'#t #'#f)]
-                   [start-fn (or (attribute start-fn) #'#f)])
-       #'(defstate* id start-fn? start-fn))]))
-
-(define-syntax defstate*
-  (syntax-rules ()
-    [(defstate id #t start-fn)
-     (struct id ()
-       #:methods gen:state
-       [(define (start state args)
-          (start-fn state args))])]
-    [(defstate id #f _)
-     (struct id ()
-       #:methods gen:state
-       [(define (start state args)
-          "starting")])]
-    [(defstate id _)
-     (struct id ())]))
+    #:datum-literals (==>)
+    [(_ name:id
+        (~or  (~once (~optional (~seq #:start start-fn*:expr)))
+              (~once (~optional (~seq #:stop stop-fn*:expr))))
+        ...)
+     #'(struct name ()
+         #:methods gen:state
+         [(define (start state args) (start-fn* state args))
+          (define (stop state) (stop-fn* state))])]))
 
 (module+ test
   (require rackunit)
 
   (defstate my-test
-    #:start (lambda (state args) "custom starting"))
-  (defstate my-test-2)
+    #:start (lambda (state args) "custom starting")
+    #:stop (lambda (state) "custom stopping"))
+  (defstate my-test-2
+    #:stop (lambda (state) "stopping")
+    #:start (lambda (state args) "starting"))
+  ;; (defstate my-test-2)
   
   (define m-1 (my-test))
   (define m-2 (my-test-2))
 
+  (check-eq? "custom stopping" (stop m-1))
   (check-eq? "custom starting" (start m-1 #f))
+  (check-eq? "stopping" (stop m-2))
   (check-eq? "starting" (start m-2 #f)))
